2746e71cb4bf4a0c952ad75f52bf6cb3280e3048
smugii23 <smugii0203@gmail.com>
2025-01-26 12:57:02 -0800

    Added count feature

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-print (file_contents)
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    print(text)
+    num_words = count_words(text)
+    print(num_words)
+
+
+def get_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+main()
